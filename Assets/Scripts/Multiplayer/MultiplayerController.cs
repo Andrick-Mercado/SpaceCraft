34aca90e17375d437c9b2a19861b437fec0b4780
Andrick Mercado <97013965+Andrick-Mercado@users.noreply.github.com>
2022-03-09 22:49:50 -0800

    Terrain now syncs
    My current random generation needs to implement seeds for the user to see same terrain

--- Assets/Scripts/Multiplayer/MultiplayerController.cs
+++ Assets/Scripts/Multiplayer/MultiplayerController.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class MultiplayerController : MonoBehaviourPunCallbacks
 {
@@ -11,10 +13,20 @@ public class MultiplayerController : MonoBehaviourPunCallbacks
     
     
     public GameObject playerPrefab;
+    public GameObject terrainPrefab;
+    public PGCTerrain terrainScript;
+
+    private PhotonView _photonView;
+
+    private void Awake()
+    {
+        _photonView = PhotonView.Get(this);
+    }
 
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(newRoomName.text);
+        
     }
 
     public void JoinRoom()
@@ -27,5 +39,16 @@ public class MultiplayerController : MonoBehaviourPunCallbacks
         Debug.Log("Joined Lobby");
         Vector3 randomPosition = new Vector3(Random.Range(-5, 5), 2, Random.Range(-5, 5));
         PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
-}
+        if (PhotonNetwork.IsMasterClient)
+        {
+            //make seed here and input it into method below, 
+            _photonView.RPC("changeTerrainBuild", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    private void changeTerrainBuild()//take seed and create same terrain here
+    {
+        terrainScript.GenerateTerrainData(true);
+    }
 }
