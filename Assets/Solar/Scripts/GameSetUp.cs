0319d2bc6023f21e0a91851a961da384936f3786
CarterAndrews <andrewscart@gmail.com>
2022-03-24 16:02:15 -0700

    revamp of character controller and procedural generation systems

---
+++ Assets/Solar/Scripts/GameSetUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetUp : MonoBehaviour {
+	public enum StartCondition { InShip, OnBody }
+
+	public StartCondition startCondition;
+	public CelestialBody startBody;
+
+	/*void Start () {
+		PlayerController player = FindObjectOfType<PlayerController> ();
+		Vector3 pointAbovePlanet = startBody.transform.position + Vector3.right * startBody.radius * 1.1f;
+		player.transform.position = pointAbovePlanet;
+		player.SetVelocity (startBody.initialVelocity);
+		
+		}*/
+	}
